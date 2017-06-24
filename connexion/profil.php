93616f50987c4ca24660dd571141b55bb9c1abbf
pierre <pierre.hamonic71@gmail.com>
2017-06-24 22:12:50 +0200

    Diverses corrections profil

--- connexion/profil.php
+++ connexion/profil.php
@@ -1,21 +1,21 @@
 <?php
 	session_start();
-	
+
 	include('../includes/init_session.php');
-	
+
 	// Redirection si admin
 	if (isset($_SESSION['connected']) AND $_SESSION['connected'] == true AND $_SESSION['identifiant'] == "admin")
 		header('location: ../administration/administration.php');
-	
+
 	if ($_SESSION['connected'] == false)
 		header('location: ../index.php');
-	
+
 	if (!isset($_SESSION['wrong_password']))
 		$_SESSION['wrong_password'] = NULL;
-	
+
 	if (!isset($_SESSION['preferences_updated']))
 		$_SESSION['preferences_updated'] = NULL;
-	
+
 	if ($_GET['user'] != $_SESSION['identifiant'])
 		header('location: ../connexion/profil.php?user=' . $_SESSION['identifiant'] . '');
 ?>
@@ -31,19 +31,19 @@
 		<meta name="description" content="Bienvenue sur Inside CGI, le portail interne au seul vrai CDS Finance" />
 		<meta name="keywords" content="Inside CGI, portail, CDS Finance" />
     </head>
-	
-	<body>	
-	
-		<header> 
+
+	<body>
+
+		<header>
 			<div class="main_title">
 				Profil
 			</div>
-			
+
 			<div class="mask">
 				<div class="triangle"></div>
 			</div>
 		</header>
-		
+
 		<section>
 			<aside>
 				<!-- Boutons d'action -->
@@ -52,39 +52,39 @@
 					$back = true;
 					$ideas = true;
 					$bug = true;
-					
+
 					include('../includes/aside.php');
 				?>
 			</aside>
-		
+
 			<article class="article_portail">
 				<!-- Gestion pseudo -->
-				<div class="categorie_profil">					
+				<div class="categorie_profil">
 					<div class="titre_profil">
 						Changement de pseudo
 					</div>
-					
+
 					<div class="contenu_profil">
 						<p class="actual">Pseudo actuel : <span class="pseudo"><?php echo $_SESSION['full_name']; ?></span></p>
-							
+
 						<form method="post" action="change_pseudo.php" class="form_pseudo">
 							<input type="text" name="new_pseudo" placeholder="Nouveau pseudo" maxlength="255" class="monoligne_profil" required />
 							<input type="submit" name="saisie_pseudo" value="Valider" class="saisie_valider_pseudo" />
 						</form>
 					</div>
 				</div>
-				
+
 				<!-- Gestion avatar -->
 				<div class="categorie_profil">
 					<div class="titre_profil">
 						Avatar
 					</div>
-					
+
 					<div class="contenu_profil">
 						<div class="zone_avatar">
 							<?php
 								include('../includes/appel_bdd.php');
-								
+
 								$reponse = $bdd->query('SELECT identifiant, full_name, avatar FROM users WHERE identifiant="' . $_SESSION['identifiant'] . '"');
 								$donnees = $reponse->fetch();
 
@@ -93,18 +93,18 @@
 
 								$reponse->closeCursor();
 							?>
-							
-							<form method="post" action="avatar.php" enctype="multipart/form-data" runat="server">					
+
+							<form method="post" action="avatar.php" enctype="multipart/form-data" runat="server">
 								<input type="hidden" name="MAX_FILE_SIZE" value="8388608" />
-								
+
 								<span class="zone_parcourir_avatar">+<input type="file" accept="image/*" name="avatar" class="bouton_parcourir_avatar" onchange="loadFile(event)" /></span>
-								
+
 								<div class="mask_avatar">
 									<img id="output" class="avatar"/>
 								</div>
 
-								<input type="submit" name="post_avatar" value="Envoyer" class="saisie_envoyer_avatar" />			
-								<input type="submit" name="delete_avatar" value="Supprimer" class="saisie_envoyer_avatar" />			
+								<input type="submit" name="post_avatar" value="Envoyer" class="saisie_envoyer_avatar" />
+								<input type="submit" name="delete_avatar" value="Supprimer" class="saisie_envoyer_avatar" />
 							</form>
 						</div>
 					</div>
@@ -115,7 +115,7 @@
 					<div class="titre_profil">
 						Changement de mot de passe
 					</div>
-					
+
 					<div class="contenu_profil">
 						<?php
 							if (isset($_SESSION['wrong_password']) AND $_SESSION['wrong_password'] == true)
@@ -133,7 +133,7 @@
 								$_SESSION['wrong_password'] = NULL;
 							}
 						?>
-						
+
 						<form method="post" action="change_mdp.php" class="form_pseudo">
 							<input type="password" name="old_password" placeholder="Ancien mot de passe" maxlength="100" class="monoligne_profil_2" required />
 							<input type="password" name="new_password" placeholder="Nouveau mot de passe" maxlength="100" class="monoligne_profil_2" required />
@@ -148,8 +148,8 @@
 					<div class="titre_profil">
 						Mes préférences
 					</div>
-					
-					<div class="contenu_profil">	
+
+					<div class="contenu_profil">
 						<form method="post" action="preferences.php" class="form_preference">
 							<?php
 								if (isset($_SESSION['preferences_updated']) AND $_SESSION['preferences_updated'] == false)
@@ -167,15 +167,15 @@
 									$_SESSION['preferences_updated'] = NULL;
 								}
 							?>
-						
+
 							<div class="titre_preference">
 								Choix de la vue par défaut Movie House
 							</div>
-							
+
 							<div class="contenu_preference">
 								<?php
 									switch ($_SESSION['view_movie_house'])
-									{											
+									{
 										case "D":
 											echo '<input id="synthese" type= "radio" name="movie_house_view" value="S" required />';
 											echo '<label for="synthese">Synthèse</label>';
@@ -183,8 +183,8 @@
 											echo '<input id="detail" type= "radio" name="movie_house_view" value="D" checked required />';
 											echo '<label for="detail">Détails</label>';
 											echo '<br />';
-											break;	
-											
+											break;
+
 										case "S":
 										default:
 											echo '<input id="synthese" type= "radio" name="movie_house_view" value="S" checked required />';
@@ -197,13 +197,13 @@
 									}
 								?>
 							</div>
-							
+
 							<div class="titre_preference">
 								Affichage de la date du jour dans la liste des films
 							</div>
-							
-							<div class="contenu_preference">	
-							
+
+							<div class="contenu_preference">
+
 								<?php
 									switch ($_SESSION['today_movie_house'])
 									{
@@ -211,7 +211,7 @@
 											echo '<input id="afficher" type="checkbox" name="affiche_date" checked />';
 											echo '<label for="afficher">Afficher</label>';
 											break;
-											
+
 										case "N":
 										default:
 											echo '<input id="afficher" type="checkbox" name="affiche_date" />';
@@ -225,45 +225,51 @@
 						</form>
 					</div>
 				</div>
-				
+
 				<!-- Gestion statistiques -->
 				<div class="categorie_profil">
 					<div class="titre_profil">
 						Mes contributions <span class="pseudo">Reference Guide</span>
 					</div>
-					
+
 					<?php
 						echo '<div class="contenu_profil">';
-							// Nombre de publications
 							include('../includes/appel_bdd.php');
-							
+
+							// Nombre de publications
 							$reponse = $bdd->query('SELECT COUNT(id) AS nb_publications FROM reference_guide WHERE author = "' . $_SESSION['identifiant'] . '"');
 							$donnees = $reponse->fetch();
-							echo '<p class="actual">Nombre de publications : <span class="pseudo">' . $donnees['nb_publications'] . '</span></p>';											
+							echo '<p class="actual">Nombre de publications : <span class="pseudo">' . $donnees['nb_publications'] . '</span></p>';
 							$reponse->closeCursor();
-							
+
 							// Nombre de votes utiles
 							//echo '<p class="actual">Nombre de votes "utile" : <span class="pseudo">NB ICI</span></p>';
-							
+
 							// Nombre de MAJ/SUP en attente
 							//echo '<p class="actual">Demandes de mise à jour / suppression en attente : <span class="pseudo">NB ICI</span></p>';
+
+							// Nombre d'idées
+							$reponse = $bdd->query('SELECT COUNT(id) AS nb_idees FROM ideas WHERE author = "' . $_SESSION['identifiant'] . '"');
+							$donnees = $reponse->fetch();
+							echo '<p class="actual">Nombre d\'idées soumises : <span class="pseudo">' . $donnees['nb_idees'] . '</span></p>';
+							$reponse->closeCursor();
 						echo '</div>';
 					?>
 				</div>
 			</article>
 		</section>
-		
+
 		<footer>
 			<?php include('../includes/footer.php'); ?>
 		</footer>
-		
+
 		<script type="text/javascript">
-			var loadFile = function(event) 
+			var loadFile = function(event)
 			{
 				var output = document.getElementById('output');
 				output.src = URL.createObjectURL(event.target.files[0]);
 			};
 		</script>
     </body>
-	
+
 </html>
