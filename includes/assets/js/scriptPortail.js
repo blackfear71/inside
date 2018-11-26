850d82252e5540d33ef13901f07568fd76077337
pierre <pierre.hamonic71@gmail.com>
2018-11-26 19:48:18 +0100

    Nouveau style messages missions

--- includes/assets/js/scriptPortail.js
+++ includes/assets/js/scriptPortail.js
@@ -1,7 +1,7 @@
 // Au chargement du document complet
 $(window).load(function()
 {
-  // On lance Masonry et le scroll après avoir chargé les images
+  // On lance Masonry après avoir chargé les images
   $('.menu_portail').masonry({
     // Options
     itemSelector: '.lien_portail',
@@ -13,4 +13,14 @@ $(window).load(function()
 
   // On associe une classe pour y ajouter une transition dans le css
   $('.menu_portail').addClass('masonry');
+
+  // On lance Masonry après avoir chargé les images
+  $('.zone_messages_missions').masonry({
+    // Options
+    itemSelector: '.zone_resume_mission',
+    columnWidth: 500,
+    fitWidth: true,
+    gutter: 20,
+    horizontalOrder: true
+  });
 });
