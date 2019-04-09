cadade986e9da909615a3188caa0cf863f6d2980
pierre <pierre.hamonic71@gmail.com>
2019-04-09 20:24:42 +0200

    Affichage des non votants dans tous les cas

--- portail/foodadvisor/vue/vue_propositions.php
+++ portail/foodadvisor/vue/vue_propositions.php
@@ -32,7 +32,7 @@
   {
     echo '<div class="zone_propositions">';
       // Bande à part et votants restants
-      if (!empty($solos) OR (!empty($sansPropositions) AND date("N") <= 5 AND date("H") < 13))
+      if (!empty($solos) OR !empty($sansPropositions))
       {
         echo '<div id="zone_solo_no_votes">';
           // Bande à part
@@ -58,28 +58,28 @@
           }
 
           // Utilisateurs sans votes
-          if (date("N") <= 5 AND date("H") < 13)
+          if (!empty($sansPropositions))
           {
-            if (!empty($sansPropositions))
-            {
-              echo '<div class="zone_proposition_solo_no_votes" id="zone_no_votes">';
+            echo '<div class="zone_proposition_solo_no_votes" id="zone_no_votes">';
+              if (date("N") <= 5 AND date("H") < 13)
                 echo '<div class="titre_solo">Ils n\'ont pas encore fait de choix</div>';
+              else
+                echo '<div class="titre_solo">Ils n\'ont pas fait de choix aujourd\'hui</div>';
 
-                foreach ($sansPropositions as $userNoChoice)
-                {
-                  echo '<div class="zone_no_vote">';
-                    // Avatar
-                    if (!empty($userNoChoice->getAvatar()))
-                      echo '<img src="../../includes/images/profil/avatars/' . $userNoChoice->getAvatar() . '" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
-                    else
-                      echo '<img src="../../includes/icons/common/default.png" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
+              foreach ($sansPropositions as $userNoChoice)
+              {
+                echo '<div class="zone_no_vote">';
+                  // Avatar
+                  if (!empty($userNoChoice->getAvatar()))
+                    echo '<img src="../../includes/images/profil/avatars/' . $userNoChoice->getAvatar() . '" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
+                  else
+                    echo '<img src="../../includes/icons/common/default.png" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
 
-                    // Pseudo
-                    echo '<div class="pseudo_no_vote">' . $userNoChoice->getPseudo() . '</div>';
-                  echo '</div>';
-                }
-              echo '</div>';
-            }
+                  // Pseudo
+                  echo '<div class="pseudo_no_vote">' . $userNoChoice->getPseudo() . '</div>';
+                echo '</div>';
+              }
+            echo '</div>';
           }
         echo '</div>';
       }
