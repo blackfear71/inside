a1d80a3653ec6fdf17eec907eda4a941e3096196
pierre <pierre.hamonic71@gmail.com>
2019-04-09 19:37:21 +0200

    Conditionnement utilisateurs sans votes sur la date

--- portail/foodadvisor/vue/vue_propositions.php
+++ portail/foodadvisor/vue/vue_propositions.php
@@ -284,25 +284,28 @@
       }
 
       // Utilisateurs sans votes
-      if (!empty($sansPropositions))
+      if (date("N") <= 5 AND date("H") < 13)
       {
-        echo '<div class="zone_proposition_top" id="zone_solo">';
-          echo '<div class="titre_solo">Ils n\'ont pas encore fait de choix</div>';
+        if (!empty($sansPropositions))
+        {
+          echo '<div class="zone_proposition_top">';
+            echo '<div class="titre_solo">Ils n\'ont pas encore fait de choix</div>';
 
-          foreach ($sansPropositions as $userNoChoice)
-          {
-            echo '<div class="zone_no_vote">';
-              // Avatar
-              if (!empty($userNoChoice->getAvatar()))
-                echo '<img src="../../includes/images/profil/avatars/' . $userNoChoice->getAvatar() . '" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
-              else
-                echo '<img src="../../includes/icons/common/default.png" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
+            foreach ($sansPropositions as $userNoChoice)
+            {
+              echo '<div class="zone_no_vote">';
+                // Avatar
+                if (!empty($userNoChoice->getAvatar()))
+                  echo '<img src="../../includes/images/profil/avatars/' . $userNoChoice->getAvatar() . '" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
+                else
+                  echo '<img src="../../includes/icons/common/default.png" alt="avatar" title="' . $userNoChoice->getPseudo() . '" class="avatar_no_vote" />';
 
-              // Pseudo
-              echo '<div class="pseudo_no_vote">' . $userNoChoice->getPseudo() . '</div>';
-            echo '</div>';
-          }
-        echo '</div>';
+                // Pseudo
+                echo '<div class="pseudo_no_vote">' . $userNoChoice->getPseudo() . '</div>';
+              echo '</div>';
+            }
+          echo '</div>';
+        }
       }
     echo '</div>';
   }
