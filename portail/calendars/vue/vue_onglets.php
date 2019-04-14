3c7eeee41a91d8a18d05696add69fc85567c6edd
pierre <pierre.hamonic71@gmail.com>
2019-04-14 03:09:26 +0200

    Gestion absence de calendriers

--- portail/calendars/vue/vue_onglets.php
+++ portail/calendars/vue/vue_onglets.php
@@ -7,52 +7,60 @@
     else
       echo '<a href="calendars.php?action=goConsulterAnnexes" class="annexes inactive">Annexes</a>';
 
-    $i            = 0;
-    $previousYear = $onglets[0];
-    $lastYear     = true;
-
-    foreach ($onglets as $year)
+    if (!empty($onglets))
     {
-      // Année inexistante (première ou au milieu)
+      $i            = 0;
+      $previousYear = $onglets[0];
+      $lastYear     = true;
+
+      foreach ($onglets as $year)
+      {
+        // Année inexistante (première ou au milieu)
+        if ($_GET['action'] != "goConsulterAnnexes")
+        {
+          if ($lastYear != false AND $anneeExistante == false AND (($_GET['year'] < $previousYear AND $_GET['year'] > $year) OR $_GET['year'] > $onglets[0]))
+          {
+            if ($i % 2 == 0)
+              echo '<span class="year active margin_right">' . $_GET['year'] . '</span>';
+            else
+              echo '<span class="year active">' . $_GET['year'] . '</span>';
+
+            $lastYear = false;
+            $i++;
+          }
+        }
+
+        // Année existante
+        if ($i % 2 == 0)
+        {
+          if (isset($_GET['year']) AND $year == $_GET['year'])
+            echo '<span class="year active margin_right">' . $year . '</span>';
+          else
+            echo '<a href="calendars.php?year=' . $year . '&action=goConsulter" class="year inactive margin_right">' . $year . '</a>';
+        }
+        else
+        {
+          if (isset($_GET['year']) AND $year == $_GET['year'])
+            echo '<span class="year active">' . $year . '</span>';
+          else
+            echo '<a href="calendars.php?year=' . $year . '&action=goConsulter" class="year inactive">' . $year . '</a>';
+        }
+
+        $previousYear = $year;
+        $i++;
+      }
+
+      // Année inexistante (dernière)
       if ($_GET['action'] != "goConsulterAnnexes")
       {
-        if ($lastYear != false AND $anneeExistante == false AND (($_GET['year'] < $previousYear AND $_GET['year'] > $year) OR $_GET['year'] > $onglets[0]))
-        {
-          if ($i % 2 == 0)
-            echo '<span class="year active margin_right">' . $_GET['year'] . '</span>';
-          else
-            echo '<span class="year active">' . $_GET['year'] . '</span>';
-
-          $lastYear = false;
-          $i++;
-        }
+        if ($lastYear == true AND $anneeExistante == false)
+          echo '<span class="year active">' . $_GET['year'] . '</span>';
       }
-
-      // Année existante
-      if ($i % 2 == 0)
-      {
-        if (isset($_GET['year']) AND $year == $_GET['year'])
-          echo '<span class="year active margin_right">' . $year . '</span>';
-        else
-          echo '<a href="calendars.php?year=' . $year . '&action=goConsulter" class="year inactive margin_right">' . $year . '</a>';
-      }
-      else
-      {
-        if (isset($_GET['year']) AND $year == $_GET['year'])
-          echo '<span class="year active">' . $year . '</span>';
-        else
-          echo '<a href="calendars.php?year=' . $year . '&action=goConsulter" class="year inactive">' . $year . '</a>';
-      }
-
-      $previousYear = $year;
-      $i++;
     }
-
-    // Année inexistante (dernière)
-    if ($_GET['action'] != "goConsulterAnnexes")
+    else
     {
-      if ($lastYear == true AND $anneeExistante == false)
-        echo '<span class="year active">' . $_GET['year'] . '</span>';
+      if ($_GET['action'] != "goConsulterAnnexes")
+        echo '<span class="year active margin_right">' . $_GET['year'] . '</span>';
     }
   echo '</div>';
 ?>
