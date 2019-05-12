454a9fe96c239ef0d0e1af1f1edb5ad4f8d82eae
pierre <pierre.hamonic71@gmail.com>
2019-05-12 16:14:23 +0200

    Revue page et JS Index

--- connexion/vue/vue_index.php
+++ connexion/vue/vue_index.php
@@ -19,61 +19,60 @@
       ?>
 
       <article class="article_index">
-        <div class="left_index">
-          <img src="includes/icons/common/inside_index.png" alt="inside_index" class="logo_index" />
-
-          <div class="categories_index">
-            <span class="zone_logo_categories" id="logo1">
-              <img src="includes/icons/common/movie_house.png" alt="movie_house_grey" class="logo_categories" />
-            </span>
-            <span class="zone_logo_categories" id="logo2">
-              <img src="includes/icons/common/food_advisor.png" alt="food_advisor_grey" class="logo_categories" />
-            </span>
-            <span class="zone_logo_categories" id="logo3">
-              <img src="includes/icons/common/expense_center.png" alt="expense_center_grey" class="logo_categories" />
-            </span>
-            <span class="zone_logo_categories" id="logo4">
-              <img src="includes/icons/common/collector.png" alt="collector_grey" class="logo_categories" />
-            </span>
-            <span class="zone_logo_categories" id="logo5">
-              <img src="includes/icons/common/calendars.png" alt="calendars_grey" class="logo_categories" />
-            </span>
-            <span class="zone_logo_categories" id="logo6">
-              <img src="includes/icons/common/petits_pedestres.png" alt="petits_pedestres_grey" class="logo_categories" />
-            </span>
-            <span class="zone_logo_categories" id="logo7">
-              <img src="includes/icons/common/missions.png" alt="missions_grey" class="logo_categories" />
-            </span>
-            <!--<span class="zone_logo_categories" id="logo8">
-              <img src="includes/icons/common/event_manager.png" alt="event_manager_grey" class="logo_categories" />
-            </span>-->
-          </div>
-        </div>
-
-        <div class="right_index">
-          <div class="bandeau_index">
-            <!-- Connexion -->
-            <form method="post" action="index.php?action=doConnecter" class="form_index">
-              <input type="text" name="login" placeholder="Identifiant" maxlength="100" class="monoligne_index" id="focus_identifiant" required />
-              <input type="password" name="mdp" placeholder="Mot de passe" maxlength="100" class="monoligne_index" required />
-              <input type="submit" name="connect" value="CONNEXION" class="bouton_index" />
-            </form>
-
-            <div class="rift_index" style="width: 2px;"></div>
-          </div>
-        </div>
-
         <?php
+          echo '<div class="left_index">';
+            echo '<img src="includes/icons/common/inside_index.png" alt="inside_index" class="logo_index" />';
+
+            echo '<div class="categories_index">';
+              echo '<span class="zone_logo_categories" id="logo1">';
+                echo '<img src="includes/icons/common/movie_house.png" alt="movie_house_grey" class="logo_categories" />';
+              echo '</span>';
+              echo '<span class="zone_logo_categories" id="logo2">';
+                echo '<img src="includes/icons/common/food_advisor.png" alt="food_advisor_grey" class="logo_categories" />';
+              echo '</span>';
+              echo '<span class="zone_logo_categories" id="logo3">';
+                echo '<img src="includes/icons/common/expense_center.png" alt="expense_center_grey" class="logo_categories" />';
+              echo '</span>';
+              echo '<span class="zone_logo_categories" id="logo4">';
+                echo '<img src="includes/icons/common/collector.png" alt="collector_grey" class="logo_categories" />';
+              echo '</span>';
+              echo '<span class="zone_logo_categories" id="logo5">';
+                echo '<img src="includes/icons/common/calendars.png" alt="calendars_grey" class="logo_categories" />';
+              echo '</span>';
+              echo '<span class="zone_logo_categories" id="logo6">';
+                echo '<img src="includes/icons/common/petits_pedestres.png" alt="petits_pedestres_grey" class="logo_categories" />';
+              echo '</span>';
+              echo '<span class="zone_logo_categories" id="logo7">';
+                echo '<img src="includes/icons/common/missions.png" alt="missions_grey" class="logo_categories" />';
+              echo '</span>';
+              /*echo '<span class="zone_logo_categories" id="logo8">';
+                echo '<img src="includes/icons/common/event_manager.png" alt="event_manager_grey" class="logo_categories" />';
+              echo '</span>';*/
+            echo '</div>';
+          echo '</div>';
+
+          echo '<div class="right_index">';
+            echo '<div class="bandeau_index">';
+              // Connexion
+              echo '<form method="post" action="index.php?action=doConnecter" class="form_index">';
+                echo '<input type="text" name="login" placeholder="Identifiant" maxlength="100" class="monoligne_index" id="focus_identifiant" required />';
+                echo '<input type="password" name="mdp" placeholder="Mot de passe" maxlength="100" class="monoligne_index" required />';
+                echo '<input type="submit" name="connect" value="CONNEXION" class="bouton_index" />';
+              echo '</form>';
+
+              echo '<div class="rift_index" style="width: 2px;"></div>';
+            echo '</div>';
+          echo '</div>';
+
           if ($error_inscription == true)
             echo '<div id="inscription" class="zone_ins_pass" style="display: block; animation: none;">';
           else
             echo '<div id="inscription" class="zone_ins_pass" style="display: block;">';
-        ?>
-          <div class="bandeau_index" style="width: 100%;">
-            <!-- Inscription -->
-            <?php
+
+            echo '<div class="bandeau_index" style="width: 100%;">';
+              // Inscription
               echo '<form method="post" action="index.php?action=doDemanderInscription" class="form_index">';
-                echo '<a onclick="masquerIndex(\'inscription\')" class="close_index"><img src="includes/icons/common/close.png" alt="close" title="Fermer" class="close_img" /></a>';
+                echo '<a id="masquerInscription" class="close_index"><img src="includes/icons/common/close.png" alt="close" title="Fermer" class="close_img" /></a>';
 
                 echo '<div class="avertissement_ins_pass">';
                   echo 'Ici vous pouvez vous inscrire au site INSIDE. Il vous suffit de renseigner votre trigramme, votre pseudo ainsi qu\'un mot de passe.
@@ -87,24 +86,20 @@
                 echo '<input type="password" name="confirm_password" value="' . $_SESSION['index']['confirmation_mot_de_passe_saisi'] . '" placeholder="Confirmer le mot de passe" maxlength="100" class="monoligne_index" required />';
                 echo '<input type="submit" name="ask_inscription" value="SOUMETTRE" class="bouton_index" />';
               echo '</form>';
-            ?>
 
-            <div class="rift_index"></div>
-          </div>
-        </div>
+              echo '<div class="rift_index"></div>';
+            echo '</div>';
+          echo '</div>';
 
-        <?php
           if ($error_password == true)
             echo '<div id="password" class="zone_ins_pass" style="display: block; animation: none;">';
           else
             echo '<div id="password" class="zone_ins_pass" style="display: block;">';
-        ?>
 
-          <div class="bandeau_index" style="width: 100%;">
-            <!-- Réinitialisation mot de passe -->
-            <?php
+            echo '<div class="bandeau_index" style="width: 100%;">';
+              // Réinitialisation mot de passe
               echo '<form method="post" action="index.php?action=doDemanderMdp" class="form_index">';
-                echo '<a onclick="masquerIndex(\'password\')" class="close_index"><img src="includes/icons/common/close.png" alt="close" title="Fermer" class="close_img" /></a>';
+                echo '<a id="masquerPassword" class="close_index"><img src="includes/icons/common/close.png" alt="close" title="Fermer" class="close_img" /></a>';
 
                 echo '<div class="avertissement_ins_pass">';
                   echo 'Si vous avez perdu votre mot de passe, vous pouvez effectuer une demande de réinitialisation du mot de passe à l\'administrateur via le formulaire ci-dessous.
@@ -115,26 +110,23 @@
                 echo '<input type="text" name="login" value="' . $_SESSION['index']['identifiant_saisi_mdp'] . '" placeholder="Identifiant" maxlength="3" class="monoligne_index" id="focus_identifiant_3" required />';
     						echo '<input type="submit" name="ask_password" value="SOUMETTRE" class="bouton_index" />';
               echo '</form>';
-            ?>
 
-            <div class="rift_index"></div>
-          </div>
-        </div>
+              echo '<div class="rift_index"></div>';
+            echo '</div>';
+          echo '</div>';
+        ?>
       </article>
     </section>
 
     <footer class="footer_index">
-      <!-- Lien inscription -->
-      <a onclick="afficherIndex('inscription', 'password');" class="link_index" id="inscription_event">
-        S'inscrire
-      </a>
-
-      <!-- Lien mot de passe perdu -->
-      <a onclick="afficherIndex('password', 'inscription');" class="link_index" id="password_event">
-        Mot de passe oublié ?
-      </a>
-
       <?php
+        // Lien inscription
+        echo '<a id="afficherInscription" class="link_index">S\'inscrire</a>';
+
+        // Lien mot de passe perdu
+        echo '<a id="afficherPassword" class="link_index">Mot de passe oublié ?</a>';
+
+        // Copyright
         echo '<div class="copyright_index">© 2017-' . date("Y") . ' Inside</div>';
       ?>
     </footer>
--- includes/assets/js/scriptIndex.js
+++ includes/assets/js/scriptIndex.js
@@ -1,27 +1,43 @@
+/***************/
+/*** Actions ***/
+/***************/
 // Au chargement du document
 $(document).ready(function()
 {
+  /*** Actions au chargement ***/
   // Positionnement du curseur sur "Identifiant" pour se connecter
   $('#focus_identifiant').focus();
 
-  // Positionnement du curseur sur "Identifiant" au clic pour s'inscrire
-  $('#inscription_event').click(function()
+  /*** Actions au clic ***/
+  // Affiche la zone d'inscription + focus
+  $('#afficherInscription').click(function()
   {
+    afficherIndex('inscription', 'password');
     $('#focus_identifiant_2').focus();
   });
 
-  // Positionnement du curseur sur "Identifiant" au clic pour réinitialiser le mot de passe
-  $('#password_event').click(function()
+  // Affiche la zone de demande de réinitialisation de mot de passe + focus
+  $('#afficherPassword').click(function()
   {
+    afficherIndex('password', 'inscription');
     $('#focus_identifiant_3').focus();
   });
 
-  // Positionnement du curseur sur "Identifiant" au clic pour fermer les fenêtres
-  $('.close_img').click(function()
+  // Ferme la demande d'inscription
+  $('#masquerInscription').click(function()
   {
+    masquerIndex('inscription');
     $('#focus_identifiant').focus();
   });
 
+  // Ferme la demande de réinitialisation de mot de passe + focus
+  $('#masquerPassword').click(function()
+  {
+    masquerIndex('password');
+    $('#focus_identifiant').focus();
+  });
+
+  /*** Actions au changement ***/
   // Transforme en majuscule les caractères saisis dans les différents identifiants
   $('#focus_identifiant, #focus_identifiant_2, #focus_identifiant_3').change(function()
   {
@@ -34,6 +50,9 @@ $(document).ready(function()
   });
 });
 
+/*****************/
+/*** Fonctions ***/
+/*****************/
 // Affiche la fenêtre d'inscription ou de mot de passe perdu (en fermant l'autre)
 function afficherIndex(id_open, id_close)
 {
