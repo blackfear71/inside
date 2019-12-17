0843e883542e0003b6ecc4113bf70edec3fb54fb
pierre <pierre.hamonic71@gmail.com>
2019-12-17 01:48:44 +0100

    Mise en commun erreurs htaccess

--- includes/errors/errors.php
+++ includes/errors/errors.php
@@ -5,9 +5,33 @@
   // Feuille de style
   echo '<link rel="stylesheet" href="/inside/includes/assets/css/styleErrors.css?version=' . $last_modification_errors . '" />';
 
-  // Erreur
+  // Erreurs
   echo '<div class="error_title">';
     echo '<img src="/inside/includes/icons/common/inside_red.png" alt="inside" title="Inside" class="error_logo" />';
-    echo 'Service non disponible';
+
+    switch ($_GET['code'])
+    {
+      /*** Erreurs client ***/
+      case 403:
+        echo 'Accès interdit';
+        break;
+
+      case 404:
+        echo 'Page non trouvée';
+        break;
+
+      case 408:
+        echo 'Délai d\'attente dépassé';
+        break;
+
+      /*** Erreurs serveur ***/
+      case 500:
+        echo 'Erreur interne du serveur';
+        break;
+
+      case 503:
+        echo 'Service non disponible';
+        break;
+    }
   echo '</div>';
 ?>
