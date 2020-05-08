81dc7ed08d911050ea824e5ff084327f38911f5d
pierre <pierre.hamonic71@gmail.com>
2020-05-08 19:02:48 +0200

    Introduction celsius message vide mobile

--- portail/foodadvisor/vue/vue_foodadvisor_mobile.php
+++ portail/foodadvisor/vue/vue_foodadvisor_mobile.php
@@ -35,10 +35,13 @@
       <article>
         <?php
           // Message vide
-          echo '<div class="empty">Bonjour <strong>' . $_SESSION['user']['pseudo'] . '</strong> et bienvenue sur la version mobile du site.</div>';
+          echo '<div class="zone_celsius">';
+            echo '<img src="../../includes/icons/common/celsius.png" alt="celsius" title="Celsius" class="celsius" />';
 
-          echo '<div class="empty">Celui-ci est en cours de développement et cette section n\'est pas encore disponible. Veuillez utiliser le bouton présent en bas du site
-          pour revenir à la version classique et utiliser toutes ses fonctionnalités.</div>';
+            echo '<div class="empty"> Bonjour <strong>' . $_SESSION['user']['pseudo'] . '</strong> et bienvenue sur la version mobile du site. Je suis <strong>Celsius</strong> et je vais te guider à travers les différentes sections du site.</div>';
+
+            echo '<div class="empty">Celle-ci est en cours de développement et n\'est pas encore disponible. Tu peux utiliser le bouton présent en bas du site pour revenir à la version classique et utiliser toutes ses fonctionnalités comme avant !</div>';
+          echo '</div>';
         ?>
       </article>
     </section>
