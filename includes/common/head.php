b5f8ce9eeb43282a865bb98744a72128d3b1b700
pierre <pierre.hamonic71@gmail.com>
2020-05-14 20:47:01 +0200

    viewport clavier android

--- includes/common/head.php
+++ includes/common/head.php
@@ -26,8 +26,8 @@
   echo '<meta charset="utf-8" />';
   echo '<meta name="description" content="Bienvenue sur Inside, le portail interne au seul vrai CDS Finance" />';
   echo '<meta name="keywords" content="Inside, portail, CDS Finance" />';
-  echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
-  
+  echo '<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">';
+
   // Styles communs
   echo '<link rel="icon" type="image/png" href="/inside/favicon.png" />';
   echo '<link rel="stylesheet" href="/inside/includes/assets/css/' . $platform . '/style.css?version=' . $last_modification_css . '" />';
