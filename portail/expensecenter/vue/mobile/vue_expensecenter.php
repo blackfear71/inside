ba38a9a6ecbaab9eff718e0d14a7f9cdb9e67275
pierre <pierre.hamonic71@gmail.com>
2020-06-30 01:45:37 +0200

    Correction position nombre users

--- portail/expensecenter/vue/mobile/vue_expensecenter.php
+++ portail/expensecenter/vue/mobile/vue_expensecenter.php
@@ -179,12 +179,12 @@
                   echo '</div>';
 
                   // Parts
-                  echo '<div class="zone_depense_parts">';
+                  echo '<div class="zone_depense_users">';
                     // Image
-                    echo '<img src="../../includes/icons/expensecenter/users_grey.png" alt="users_grey" title="Parts utilisateurs" class="icone_depense" />';
+                    echo '<img src="../../includes/icons/expensecenter/users_grey.png" alt="users_grey" title="Nombre d\'utilisateurs" class="icone_depense" />';
 
                     // Nombre de parts
-                    echo '<div class="nombre_parts_depense">' . $depense->getNb_users() . '</div>';
+                    echo '<div class="nombre_users_depense">' . $depense->getNb_users() . '</div>';
                   echo '</div>';
                 echo '</div>';
               }
