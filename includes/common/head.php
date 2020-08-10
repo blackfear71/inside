317bc3a6e22f389ab36f6ee0a58b7103edcb220f
pierre <pierre.hamonic71@gmail.com>
2020-08-11 00:46:40 +0200

    Fallback local Jquery

--- includes/common/head.php
+++ includes/common/head.php
@@ -47,7 +47,9 @@
 
 <!-- Scripts communs -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/inside/includes/libraries/js/jquery-3.5.1.min.js"><\/script>')</script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/inside/includes/libraries/js/jquery-ui-1.12.1.min.js"><\/script>')</script>
 <script src="/inside/includes/assets/js/<?php echo $_SESSION['index']['plateforme']; ?>/script.js?version=<?php echo $lastModificationJs; ?>"></script>
 <script src="/inside/includes/libraries/js/jCirclize.js"></script>
 
