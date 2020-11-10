3dceda7c9d93e4375b3bcd69cbb486e311825fc2
blackfear71 <pierre.hamonic71@gmail.com>
2020-11-10 20:13:47 +0100

    Revue style profil mobile

--- portail/profil/vue/mobile/vue_infos.php
+++ portail/profil/vue/mobile/vue_infos.php
@@ -14,34 +14,37 @@
 
     echo '<img src="' . $avatarFormatted['path'] . '" alt="' . $avatarFormatted['alt'] . '" title="' . $avatarFormatted['title'] . '" class="avatar_profil" />';
 
-    // Niveau
-    echo '<div class="zone_info">';
-      echo '<img src="../../includes/icons/common/inside_red.png" alt="inside_red" class="logo_profil" />';
-      echo '<div class="texte_profil">Insider de <strong>niveau ' . $progression->getNiveau() . '</strong></div>';
-    echo '</div>';
-
-    // Expérience
-    echo '<div class="zone_info">';
-      echo '<img src="../../includes/icons/profil/experience_grey.png" alt="experience_grey" class="logo_profil" />';
-      echo '<div class="fond_experience_profil"><div class="experience_profil" style="width: ' . $progression->getPourcentage() . '%;"></div></div>';
-    echo '</div>';
-
-    // Adresse mail
-    if (!empty($profil->getEmail()))
-    {
+    // Informations
+    echo '<div class="zone_texte_infos">';
+      // Niveau
       echo '<div class="zone_info">';
-        echo '<img src="../../includes/icons/profil/mailing_red.png" alt="mailing_red" class="logo_profil" />';
-        echo '<div class="texte_profil">' . $profil->getEmail() . '</div>';
+        echo '<img src="../../includes/icons/common/inside_red.png" alt="inside_red" class="logo_profil" />';
+        echo '<div class="texte_profil">Insider de <strong>niveau ' . $progression->getNiveau() . '</strong></div>';
       echo '</div>';
-    }
 
-    // Anniversaire
-    if (!empty($profil->getAnniversary()))
-    {
+      // Expérience
       echo '<div class="zone_info">';
-        echo '<img src="../../includes/icons/profil/anniversary_grey.png" alt="anniversary_grey" class="logo_profil" />';
-        echo '<div class="texte_profil">Anniversaire le ' . formatDateForDisplay($profil->getAnniversary()) . '</div>';
+        echo '<img src="../../includes/icons/profil/experience_grey.png" alt="experience_grey" class="logo_profil" />';
+        echo '<div class="fond_experience_profil"><div class="experience_profil" style="width: ' . $progression->getPourcentage() . '%;"></div></div>';
       echo '</div>';
-    }
+
+      // Adresse mail
+      if (!empty($profil->getEmail()))
+      {
+        echo '<div class="zone_info">';
+          echo '<img src="../../includes/icons/profil/mailing_red.png" alt="mailing_red" class="logo_profil" />';
+          echo '<div class="texte_profil">' . $profil->getEmail() . '</div>';
+        echo '</div>';
+      }
+
+      // Anniversaire
+      if (!empty($profil->getAnniversary()))
+      {
+        echo '<div class="zone_info">';
+          echo '<img src="../../includes/icons/profil/anniversary_grey.png" alt="anniversary_grey" class="logo_profil" />';
+          echo '<div class="texte_profil">Anniversaire le ' . formatDateForDisplay($profil->getAnniversary()) . '</div>';
+        echo '</div>';
+      }
+    echo '</div>';
   echo '</div>';
 ?>
