1e2b3ed9c4de27bf891c6e9c5a5244e113b35532
blackfear71 <pierre.hamonic@gmail.com>
2021-06-04 02:22:21 +0200

    Debug

--- includes/assets/js/web/scriptAdmin.js
+++ includes/assets/js/web/scriptAdmin.js
@@ -142,11 +142,15 @@ $(function()
     // Affichage ou masquage de la période concernée
     if ($('#zone_affichage_periodes_vacances').css('display') == 'none')
     {
+      console.log('test 1');
+
       // Affichage de la période concernée
       showPeriodesVacances($(this), $(this).text());
     }
     else
     {
+      console.log('test 2');
+
       // Détermination si ouverture d'une autre période
       var autrePeriode = false;
       var nomFichier   = $(this).text();
@@ -163,9 +167,13 @@ $(function()
       // Affichage de la période concernée
       setTimeout(function()
       {
+        console.log(autrePeriode);
+        console.log($(this));
+        console.log(nomFichier);
+
         if (autrePeriode == true)
           showPeriodesVacances($(this), nomFichier);
-      }, 200);
+      }, 100);
     }
   });
 
