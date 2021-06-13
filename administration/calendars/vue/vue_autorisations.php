5ecf36751f8d6cc423e09bd3de07fbefc5846846
blackfear71 <pierre.hamonic@gmail.com>
2021-06-13 15:07:58 +0200

    Correction style gestion autorisations admin

--- administration/calendars/vue/vue_autorisations.php
+++ administration/calendars/vue/vue_autorisations.php
@@ -20,14 +20,14 @@
               {
                 echo '<div id="bouton_autorisation_' . $autorisation->getIdentifiant() . '" class="switch_autorisation switch_checked">';
                   echo '<input id="autorisation_' . $autorisation->getIdentifiant() . '" type="checkbox" name="autorization[' . $autorisation->getIdentifiant() . ']" checked />';
-                  echo '<label for="autorisation_' . $autorisation->getIdentifiant() . '" class="label_switch">' . $autorisation->getPseudo() . '</label>';
+                  echo '<label for="autorisation_' . $autorisation->getIdentifiant() . '" class="label_switch">' . formatString($autorisation->getPseudo(), 100) . '</label>';
                 echo '</div>';
               }
               else
               {
                 echo '<div id="bouton_autorisation_' . $autorisation->getIdentifiant() . '" class="switch_autorisation">';
                   echo '<input id="autorisation_' . $autorisation->getIdentifiant() . '" type="checkbox" name="autorization[' . $autorisation->getIdentifiant() . ']" />';
-                  echo '<label for="autorisation_' . $autorisation->getIdentifiant() . '" class="label_switch">' . $autorisation->getPseudo() . '</label>';
+                  echo '<label for="autorisation_' . $autorisation->getIdentifiant() . '" class="label_switch">' . formatString($autorisation->getPseudo(), 100) . '</label>';
                 echo '</div>';
               }
             }
