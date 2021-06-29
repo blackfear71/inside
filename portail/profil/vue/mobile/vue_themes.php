0a89e2ccd618ed1687539b79211c8ae497e8f359
blackfear71 <pierre.hamonic@gmail.com>
2021-06-29 19:17:01 +0200

    Misqe en valeur titre changement police

--- portail/profil/vue/mobile/vue_themes.php
+++ portail/profil/vue/mobile/vue_themes.php
@@ -26,7 +26,7 @@
 
     // Exemple de texte
     echo '<div id="exemple_police" style="font-family: ' . $_SESSION['user']['font'] . ',Verdana, sans-serif;">';
-      echo '<p>Exemple de texte</p>';
+      echo '<p><strong>Exemple de texte</strong></p>';
 
       echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
       ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec
--- portail/profil/vue/web/vue_themes.php
+++ portail/profil/vue/web/vue_themes.php
@@ -19,7 +19,7 @@
   echo '</form>';
 
   echo '<div id="exemple_police" style="font-family: ' . $_SESSION['user']['font'] . ',Verdana, sans-serif;">';
-    echo '<p>Exemple de texte</p>';
+    echo '<p><strong>Exemple de texte</strong></p>';
 
     echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
     ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec
