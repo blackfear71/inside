f0d85c6bb26c1bbcad353d7f3587b639d8a489b0
Pierre <pierre.hamonic71@gmail.com>
2023-02-13 20:57:06 +0100

    Indentation évènements

--- portail/eventmanager/eventmanager.php
+++ portail/eventmanager/eventmanager.php
@@ -1,51 +1,51 @@
 <?php
-  /****************************
-  ******* Event Manager *******
-  *****************************
-  Fonctionnalités :
-  - Consultation des évènements
-  ****************************/
+    /****************************
+    ******* Event Manager *******
+    *****************************
+    Fonctionnalités :
+    - Consultation des évènements
+    ****************************/
 
-  // Fonctions communes
-  include_once('../../includes/functions/metier_commun.php');
-  include_once('../../includes/functions/physique_commun.php');
-  include_once('../../includes/functions/fonctions_dates.php');
+    // Fonctions communes
+    include_once('../../includes/functions/metier_commun.php');
+    include_once('../../includes/functions/physique_commun.php');
+    include_once('../../includes/functions/fonctions_dates.php');
 
-  // Contrôles communs Utilisateur
-  controlsUser();
+    // Contrôles communs Utilisateur
+    controlsUser();
 
-  // Modèle de données
-  include_once('modele/metier_eventmanager.php');
+    // Modèle de données
+    include_once('modele/metier_eventmanager.php');
 
-  // Appels métier
-  switch ($_GET['action'])
-  {
-    case 'goConsulter':
-      // Lecture des données par le modèle
-      break;
+    // Appels métier
+    switch ($_GET['action'])
+    {
+        case 'goConsulter':
+            // Lecture des données par le modèle
+            break;
 
-    default:
-      // Contrôle action renseignée URL
-      header('location: eventmanager.php?action=goConsulter');
-      break;
-  }
+        default:
+            // Contrôle action renseignée URL
+            header('location: eventmanager.php?action=goConsulter');
+            break;
+    }
 
-  // Traitements de sécurité avant la vue
-  switch ($_GET['action'])
-  {
-    case 'goConsulter':
-      break;
+    // Traitements de sécurité avant la vue
+    switch ($_GET['action'])
+    {
+        case 'goConsulter':
+            break;
 
-    default:
-      break;
-  }
+        default:
+            break;
+    }
 
-  // Redirection affichage
-  switch ($_GET['action'])
-  {
-    case 'goConsulter':
-    default:
-      include_once('vue/vue_eventmanager.php');
-      break;
-  }
+    // Redirection affichage
+    switch ($_GET['action'])
+    {
+        case 'goConsulter':
+        default:
+            include_once('vue/vue_eventmanager.php');
+            break;
+    }
 ?>
--- portail/eventmanager/vue/vue_eventmanager.php
+++ portail/eventmanager/vue/vue_eventmanager.php
@@ -1,78 +1,79 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <!-- Head commun & spécifique-->
-    <?php
-      $titleHead       = 'Event Manager';
-      $styleHead       = 'styleEM.css';
-      $scriptHead      = '';
-      $angularHead     = false;
-      $chatHead        = true;
-      $datepickerHead  = false;
-      $masonryHead     = false;
-      $exifHead        = false;
-      $html2canvasHead = false;
-      $jqueryCsv       = false;
-
-      include('../../includes/common/head.php');
-    ?>
-  </head>
-
-	<body>
-		<!-- Entête -->
-		<header>
-      <?php
-        $title = 'Event Manager';
-
-        include('../../includes/common/web/header.php');
-			  include('../../includes/common/web/onglets.php');
-      ?>
-		</header>
-
-    <!-- Contenu -->
-		<section>
-      <!-- Messages d'alerte -->
-      <?php include('../../includes/common/alerts.php'); ?>
-
-      <!-- Déblocage succès -->
-      <?php include('../../includes/common/success.php'); ?>
-
-			<article>
+    <head>
+        <!-- Head commun & spécifique-->
         <?php
-          /********************/
-          /* Boutons missions */
-          /********************/
-          $zoneInside = 'article';
-          include('../../includes/common/missions.php');
+            $titleHead       = 'Event Manager';
+            $styleHead       = 'styleEM.css';
+            $scriptHead      = '';
+            $angularHead     = false;
+            $chatHead        = true;
+            $datepickerHead  = false;
+            $masonryHead     = false;
+            $exifHead        = false;
+            $html2canvasHead = false;
+            $jqueryCsv       = false;
 
-          /*******************/
-          /* Chargement page */
-          /*******************/
-          echo '<div class="zone_loading_page">';
-            echo '<div id="loading_page" class="loading_page"></div>';
-          echo '</div>';
-
-          echo '<div class="entete_event">';
-            echo 'Les évènements à venir';
-          echo '</div>';
-
-          echo '<div class="entete_event">';
-            echo 'Tous les évènements';
-          echo '</div>';
-
-          echo '<div class="entete_event">';
-            echo 'Le référentiel';
-          echo '</div>';
+            include('../../includes/common/head.php');
         ?>
-			</article>
+    </head>
 
-      <!-- Chat -->
-      <?php include('../../includes/common/chat/chat.php'); ?>
-		</section>
+    <body>
+        <!-- Entête -->
+        <header>
+            <?php
+                $title = 'Event Manager';
 
-		<!-- Pied de page -->
-		<footer>
-			<?php include('../../includes/common/web/footer.php'); ?>
-		</footer>
-  </body>
+                include('../../includes/common/web/header.php');
+                include('../../includes/common/web/onglets.php');
+            ?>
+        </header>
+
+        <!-- Contenu -->
+        <section>
+            <!-- Messages d'alerte -->
+            <?php include('../../includes/common/alerts.php'); ?>
+
+            <!-- Déblocage succès -->
+            <?php include('../../includes/common/success.php'); ?>
+
+            <article>
+                <?php
+                    /********************/
+                    /* Boutons missions */
+                    /********************/
+                    $zoneInside = 'article';
+
+                    include('../../includes/common/missions.php');
+
+                    /*******************/
+                    /* Chargement page */
+                    /*******************/
+                    echo '<div class="zone_loading_page">';
+                        echo '<div id="loading_page" class="loading_page"></div>';
+                    echo '</div>';
+
+                    echo '<div class="entete_event">';
+                        echo 'Les évènements à venir';
+                    echo '</div>';
+
+                    echo '<div class="entete_event">';
+                        echo 'Tous les évènements';
+                    echo '</div>';
+
+                    echo '<div class="entete_event">';
+                        echo 'Le référentiel';
+                    echo '</div>';
+                ?>
+            </article>
+
+            <!-- Chat -->
+            <?php include('../../includes/common/chat/chat.php'); ?>
+        </section>
+
+        <!-- Pied de page -->
+        <footer>
+            <?php include('../../includes/common/web/footer.php'); ?>
+        </footer>
+    </body>
 </html>
